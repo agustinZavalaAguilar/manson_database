7634a818d56725ac332e11ec9f42597553696215
agustinZavalaAguilar <88766456+agustinZavalaAguilar@users.noreply.github.com>
2024-03-13 21:17:07 +0100

    feed scroll down menu with trimesters from API

--- script/script.js
+++ script/script.js
@@ -1,16 +1,46 @@
 
+//This section bulds API request following the user's choices-----------------------------------
 
-
-let param = "select=region%2C%20nombre_d_operations_actives%2C%20centroid&limit=20";
+let param = "select=trimestre&group_by=trimestre&order_by=trimestre%20%20DESC%20&limit=20";
     
-
+let raw_trimesters=[];
 
 fetch('https://data.enedis.fr/api/explore/v2.1/catalog/datasets/autoconsommation-collective-maille-region/records?'+ param)
 
-        .then(response => {return response.json()})
-        .then((data)=>{
-            console.log(data);
-            //let fill1 = document.getElementById('para1');
-            //fill1.innerHTML=data.value;
+  .then(response => {return response.json()})
+  .then((data)=>{
+      console.log(data.results[0].trimestre);
+//Extract an array from the response dataset      
+        for (var i=0; i<data.results.length; i++){
+        raw_trimesters.push(data.results[i].trimestre);
+      };
+// Trim useless characters from the raw trimester array
+const trimmed_trimesters = raw_trimesters.map(dateString => dateString.substring(0, dateString.indexOf('T')));
+
+
+// This section feeds the trimesters to the scroll down menu---------------------------------------------------------
+
+  function populateMenu() {
+      const selectMenu = document.getElementById("scroll_menu");
+    
+      trimmed_trimesters.forEach((option, index) => {
+        const optionElement = document.createElement("option");
+        optionElement.value = index; // Set value as index
+        optionElement.text = option; // Set text as option
+        selectMenu.appendChild(optionElement);
+    });
+  }
+  
+  // Call the function to populate the menu
+  populateMenu();
+
         })
+
+//region, nombre_d_operations_actives, trimestre, centroid
+
+
+ 
+
+
+
 
