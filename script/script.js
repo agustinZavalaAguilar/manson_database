264cf2336653306b3eb3f8f99ca7f211d5dda641
agustinZavalaAguilar <88766456+agustinZavalaAguilar@users.noreply.github.com>
2024-03-13 13:53:56 +0100

    test fetch api

--- script/script.js
+++ script/script.js
@@ -1,9 +1,16 @@
-console.log("test");
 
-fetch('https://data.enedis.fr/api/explore/v2.1/catalog/datasets/autoconsommation-collective-maille-region/records?limit=20')
+
+
+let param = "select=*&where=region%20%3D%20%27Occitanie%27&limit=20";
+    
+
+
+fetch('https://data.enedis.fr/api/explore/v2.1/catalog/datasets/autoconsommation-collective-maille-region/records?'+ param)
+
         .then(response => {return response.json()})
         .then((data)=>{
             console.log(data);
-            let fill1 = document.getElementById('para1');
-            fill1.innerHTML=data.value;
+            //let fill1 = document.getElementById('para1');
+            //fill1.innerHTML=data.value;
         })
+
